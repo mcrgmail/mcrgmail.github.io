4559140a4babc94e97d2deb7cfb086774f62b796
mcrgmail <89011941+mcrgmail@users.noreply.github.com>
2021-09-08 18:35:31 +0200

    .

--- res/show.js
+++ res/show.js
@@ -1,12 +1,18 @@
 function showFunction(id) {
-  var x = document.getElementById(id+'_inner');
+  var x_inner = document.getElementById(id+'_inner');
   var x_arrow = document.getElementById(id+'_arrow');
 
-  if (x.style.display === "none") {
-    x.style.display = "block";
+  if (x_inner.style.display == "none" || x_inner.style.display == "") {
+    x_inner.style.display = "block";
     x_arrow.style.transform = "translate(0%, 0%) rotate(225deg)" ;
   } else {
-    x.style.display = "none";
+    x_inner.style.display = "none";
     x_arrow.style.transform = "translate(0%, 0%) rotate(45deg)";
   }
 }
+
+document.getElementById("id_pa_wrap").addEventListener("click", function(){ showFunction("id_pa")}, false);
+document.getElementById("id_pb_wrap").addEventListener("click", function(){ showFunction("id_pb")}, false);
+document.getElementById("id_pc_wrap").addEventListener("click", function(){ showFunction("id_pc")}, false);
+document.getElementById("id_pd_wrap").addEventListener("click", function(){ showFunction("id_pd")}, false);
+document.getElementById("id_pe_wrap").addEventListener("click", function(){ showFunction("id_pe")}, false);
